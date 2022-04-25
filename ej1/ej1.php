55aa0f5b6e514be6c09c35e3148b53db87e0ec2f
Virginia-Ordoño-Bernier <viorbe20@gmail.com>
2022-04-25 17:51:09 +0200

    feat: lleva a test seleccionado

--- ej1/ej1.php
+++ ej1/ej1.php
@@ -7,11 +7,25 @@
  */
 include "config/tests_cnf.php";
 $processForm = true;
+$showTest1 = false;
+$showTest2 = false;
+$showTest3 = false;
 
 if (isset($_POST['start'])) {
-    echo('<br>xxxxx') ;
-    $selectedTest = $_POST['testSelection'];
-    echo('<br>'. $selectedTest) ;
+    $selectedTest = $_POST['selectedTest'];
+    $processForm = false;
+
+    switch ($selectedTest) {
+        case '1':
+            $showTest1 = true;
+            break;
+        case '1':
+            $showTest2 = true;
+            break;
+        case '1':
+            $showTest3 = true;
+            break;
+    }
 }
 
 
@@ -19,16 +33,39 @@ if ($processForm) {
 ?>
     <h1>Test online Autoescuela</h1>
     <h3>Selecciona el test por el que quieras comenzar.</h3>
-<form action="" method="post">
-<select name="testSelection" id="testSelection">
-    <option value="1">Test 1</option>
-    <option value="2">Test 2</option>
-    <option value="3">Test 3</option>
-</select>
-<br>
-<br>
-<button type="submit" name="start">Comenzar</button>
-</form>
+    <form action="" method="post">
+        <select name="selectedTest">
+            <option value="1">Test 1</option>
+            <option value="2">Test 2</option>
+            <option value="3">Test 3</option>
+        </select>
+        <br>
+        <br>
+        <button type="submit" name="start">Comenzar</button>
+    </form>
+<?php
+}
+
+if ($showTest1) {
+?>
+    <h1>Test 1: Permiso B</h1>
+
+<?php
+
+}
+
+if ($showTest2) {
+?>
+    <h1>Test 2: Permiso B</h1>
+
+<?php
+
+}
+
+if ($showTest3) {
+?>
+    <h1>Test 3: Permiso B</h1>
+
 <?php
 
 }
