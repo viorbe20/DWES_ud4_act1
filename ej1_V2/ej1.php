f415771d5f610a293b2843841d5dfb420c058784
Virginia-Ordoño-Bernier <viorbe20@gmail.com>
2022-04-30 21:35:42 +0200

    feat: muestra test seleccionado

--- ej1_V2/ej1.php
+++ ej1_V2/ej1.php
@@ -9,23 +9,92 @@ include "config/tests_cnf.php";
 
 $aTestSolution;
 $processForm = true;
+$showSolution = false;
+
+//Cuenta número de cookies
+$cookiesAmount = array_count_values($_COOKIE);
+$aTestSolution = getTestSolution(0, $aTests);
+
 /**
  * Dado un número de test, devuelve las respuestas de ese test
  */
-function getTestSolution($testNUmber, $aTests)
+function getTestSolution($selectedTest, $aTests)
 {
     for ($i = 0; $i < count($aTests); $i++) {
-        foreach ($aTests[$testNUmber] as $level1 => $value) {
+        foreach ($aTests[$selectedTest] as $level1 => $value) {
             if ($level1 == "Corrector") {
                 return $value;
             }
         }
     }
-
-    //return $aTestSolution;
 }
 
-$aTestSolution = getTestSolution(0, $aTests);
+/**
+ * Muestra el test seleccionado
+ */
+function showSelectedTest($selectedTest, $aTests, $showSolution)
+{
+
+    for ($i = 0; $i < count($aTests); $i++) {
+        foreach ($aTests[$selectedTest] as $level1 => $value) {
+            if ($level1 == "Preguntas") {
+                foreach ($value as $key) {
+                    $css = file_get_contents("css/style.css");
+                    echo "<style>$css</style>";
+                    //var_dump($key);
+                    echo "<div class='question'>";
+                    echo "<h3>Pregunta " . $key['Pregunta'] . "</h3>";
+
+                    //Comprueba si la foto existe
+                    if (file_exists("dir_img_test" . $selectedTest + 1 . "/img" . $key['idPregunta'] . ".jpg")) {
+                        echo "<img src=dir_img_test" . $selectedTest + 1 . "/img" . $key['idPregunta'] . ".jpg>";
+                    }
+
+                    echo "<p class='answer'>Respuestas</p>";
+
+                    //Imprime el número de  respuestas que tenga el test
+                    for ($i = 0; $i < count($key['respuestas']); $i++) {
+
+                        //Muestra aciertos
+                        if ($showSolution) {
+                            // if (($a_userAnswers[$level2]['option'] == $i) && ($a_userAnswers[$level2]['status'] == "right")) {
+                            //     $className = "right";
+                            // } elseif (($a_userAnswers[$level2]['option'] == $i) && ($a_userAnswers[$level2]['status'] == "wrong")) {
+                            //     $className = "wrong";
+                            //     $errors++;
+                            // } else {
+                            //     $className = "nsnc";
+                            //     $errors++;
+                            // }
+
+                            //Desabilita radiobutton cuando muestra resultados
+                            $disabled = "disabled";
+                        } else {
+                            $className = "";
+                            $disabled = "";
+                        }
+
+                        echo "<label class=\"$className\">" . $key['respuestas'][$i] . "</label>";
+                        $name = ($key['idPregunta'] - 1);
+                        echo "<input type=\"radio\" name=" . $name . " value=\"$i\" $disabled/>";
+                        echo ('<br>');
+                    }
+                }
+            }
+        }
+    }
+}
+
+//Tras el botón comenzar
+if (isset($_POST["start"])) {
+    $processForm = false;
+    $selectedTest = $_POST['selectedTest'];
+
+    //Creamos cookie con el número de test seleccionado
+    setcookie('currentTest', $selectedTest, time() + 36000);
+
+    showSelectedTest($selectedTest, $aTests, false);
+}
 
 //Muestra página inicial. Muestra tantos tests como tenga el array
 if ($processForm) {
